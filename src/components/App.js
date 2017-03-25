df2ca92f624e4e4d1477e03320a692b54dfb5db6
Ted Shaffer <ted@brightsign.biz>
2017-03-25 06:00:37 -0700

    initial UI - browse for file.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,10 @@
 
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+
+const {dialog} = require('electron').remote;
 
 export default class App extends Component {
 
@@ -9,16 +13,35 @@ export default class App extends Component {
     console.log("app.js::componentDidMount invoked");
   }
 
+  browseForFile() {
+    dialog.showOpenDialog({
+      properties: ['openFile']
+    }, (filePaths) => {
+      console.log('selected file: ', filePaths[0]);
+    });
+  }
+
   render() {
 
     console.log("app.js::render invoked");
 
+    let self = this;
+
     return (
       <MuiThemeProvider>
         <div>
-          <p className="autorunText">Pizza</p>
-          <p className="autorunText">Line 2</p>
-          <p className="autorunText">Line 3</p>
+          <p>Select Presentation</p>
+          <TextField
+            id='filePath'
+            ref={(c) => {
+              this.filePath = c;
+            }}
+          />
+          <FlatButton
+            disableTouchRipple={true}
+            label='Browse'
+            onTouchTap={self.browseForFile.bind(self)}
+          />
         </div>
       </MuiThemeProvider>
     );
